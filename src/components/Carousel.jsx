decfffd5ced7b1fb97ca54dfd37cc6538af7b479
Daniel Vasquez <daniel.vasquez.git@gmail.com>
2023-04-22 16:36:23 -0600

    Changing Carousel design

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,42 +1,43 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './Carousel.css'
 
-export function Carousel({ images }) {
-  const [position, setPosition] = useState(0);
+function checkImageStatus(url) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.onload = () => resolve(url);
+    img.onerror = () => reject(url);
+    img.src = url;
+  });
+}
 
-  const imgWidth = 200;
-  
-  const handleNavLeftClick = () => {
-    if (position > 0) {
-      setPosition(position - imgWidth);
-    }
-  };
-  
-  const handleNavRightClick = () => {
-    if (position < imgWidth * (images.length - 1)) {
-      setPosition(position + imgWidth);
-    }
-  };
-  
+export function Carousel({ images }) {
+  const [imageStatus, setImageStatus] = useState([]);
+
+  useEffect(() => {
+    Promise.all(images.map(checkImageStatus))
+      .then(() => {
+        setImageStatus(images.map(() => true));
+      })
+      .catch(() => {
+        setImageStatus(images.map(() => false));
+      });
+  }, [images]);
+
   return (
-    <div className="carousel-container">
-      <div className="carousel-wrapper" style={{ transform: `translateX(-${position}px)` }}>
-        {images.map((image, index) => {
-          return (
-            <img key={index} src={image} alt={`Imagen ${index + 1}`} lazy='loading' />
+    <div className="slider">
+      <div className="slides">
+        {images.map((img, index) =>
+          imageStatus[index] ? (
+            <div key={index} className="slides-img">
+              <img className="slides-img__img" src={img} alt={`Imagen ${index + 1}`} lazy='loading' />
+            </div>
+          ) : (
+            <div key={index} className="slides-img" style={{ border: '1px solid #575757' }}>
+              imagen no disponible
+            </div>
           )
-        })}
+        )}
       </div>
-      {images.length > 1 && (
-        <>
-          <button className="carousel-nav-left" onClick={handleNavLeftClick}>
-            {'<'}
-          </button>
-          <button className="carousel-nav-right" onClick={handleNavRightClick}>
-            {'>'}
-          </button>
-        </>
-      )}
     </div>
   );
 }
